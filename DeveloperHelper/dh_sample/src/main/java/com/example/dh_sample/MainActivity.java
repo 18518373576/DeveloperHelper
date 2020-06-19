332ff68b2d6abeda6ad1625b8c80e200752d5fc6
Zhang <510446914@qq.com>
2020-06-19 14:40:51 +0800

    添加ContentProvider和AIDL演示示例

---
+++ DeveloperHelper/dh_sample/src/main/java/com/example/dh_sample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dh_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 示例主要配合developerandroidx演示AIDL和ContentProvider
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
